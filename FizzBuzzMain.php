6c74e54466f8a82b8bdc03c34a2a60178892c992
huyIttos2 <huyittos2071195@gmail.com>
2019-06-14 11:30:22 +0700

    hello

---
+++ FizzBuzzMain.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dungduong
+ * Date: 10/27/2018
+ * Time: 4:42 PM
+ */
+
+include('FizzBuzz.php');
+include_once 'FizzBuzzTest.php';
+$newTest = new FizzBuzzTest();
+$newTest->testFizz3();
